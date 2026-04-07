ba6d6c9421af729323d93f886e167cc3cc162717
nb3n <hello@nben.com.np>
2026-04-07 12:49:34 +0100

    chore(category): set up record navigation on view

--- app/Filament/Resources/Categories/Pages/ViewCategory.php
+++ app/Filament/Resources/Categories/Pages/ViewCategory.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\Categories\Pages;
 use App\Filament\Resources\Categories\CategoryResource;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
+use Nben\FilamentRecordNav\Actions\NextRecordAction;
+use Nben\FilamentRecordNav\Actions\PreviousRecordAction;
+use Nben\FilamentRecordNav\Enums\NavigationPage;
 
 class ViewCategory extends ViewRecord
 {
@@ -13,6 +16,12 @@ class ViewCategory extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            PreviousRecordAction::make()
+                ->navigateTo(NavigationPage::View),
+
+            NextRecordAction::make()
+                ->navigateTo(NavigationPage::View),
+
             EditAction::make(),
         ];
     }
