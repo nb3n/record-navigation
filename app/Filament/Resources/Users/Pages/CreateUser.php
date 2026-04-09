f72db856d72afa8b5aad1c22763402c8c77e6658
nb3n <hello@nben.com.np>
2026-04-09 12:34:29 +0100

    chore(user): halt creating data

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -4,8 +4,20 @@ namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function beforeCreate(): void
+    {
+        Notification::make()
+            ->title('Now, now, these records aren\'t yours to create!')
+            ->warning()
+            ->send();
+        
+        $this->form->fill(); 
+        $this->halt();
+    }
 }
