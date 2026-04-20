69ea5d3601b3fdc9976e751669f3cfb4ab5c46e1
nb3n <hello@nben.com.np>
2026-04-20 08:24:34 +0100

    chore(user): cleaned duplicate method

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -15,16 +15,6 @@ class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
 
-    protected function beforeSave(): void
-    {
-        Notification::make()
-            ->title('Now, now, these records aren\'t yours to edit!')
-            ->warning()
-            ->send();
-
-        $this->halt();
-    }
-    
     protected function getHeaderActions(): array
     {
         return [
