98d073c810d1445eeba5af08785ef24f7829c9d4
nb3n <hello@nben.com.np>
2026-04-20 08:13:03 +0100

    chore(post): prevent user to creat new data

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -4,8 +4,20 @@ namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function beforeCreate(): void
+    {
+        Notification::make()
+            ->title('Now, now, these records aren\'t yours to create!')
+            ->warning()
+            ->send();
+
+        $this->form->fill();
+        $this->halt();
+    }
 }
