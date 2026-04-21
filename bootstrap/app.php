d462592ac999f492e744bc087d14e9307b53539a
nb3n <hello@nben.com.np>
2026-04-21 07:46:53 +0100

    chore(app): manual remove trustproxy so user maynot upload the files to s3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,13 +19,14 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->trustProxies(
-            at: '*',
-            headers: Request::HEADER_X_FORWARDED_FOR |
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO
-        );
+        // Trust Proxies
+        // $middleware->trustProxies(
+        //     at: '*',
+        //     headers: Request::HEADER_X_FORWARDED_FOR |
+        //         Request::HEADER_X_FORWARDED_HOST |
+        //         Request::HEADER_X_FORWARDED_PORT |
+        //         Request::HEADER_X_FORWARDED_PROTO
+        // );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
